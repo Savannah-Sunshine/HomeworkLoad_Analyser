50a14bdce14e6b5399205dd6a5f74c8f8f3ec656
Savannah-Sunshine <s.ella@me.com>
2022-04-12 11:29:41 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+
+# Input:
+# read a file that contains all the homework input
+#       file also has start_date and end_date
+# make it canvas & learning suite readable? ... inport from both?
+
+# organise that homework with its classes, weight, name
+#    (if it has the word project, essays, test, then it is heavier)
+
+
+
+
+#main
+#
+
+
+
+
+
+
+# Output:
+# based on command of user
+#   B:  Get the busiest week (most # of homework)
+#   B_Many:  Get the busiest weeks
+#   L:       Get the lightest week (least # of homework)
+#   L_Many:  Get the lightest/easiest weeks
+#   All:     Prints out every week with # of hw & output is colored?
+#               realitively to its "easiness"
